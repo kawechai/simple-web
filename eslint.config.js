e2f4bc8aeb89d6b483de087e955d7357cc253741
kawechai.pu <kawechai.pu@planbmedia.co.th>
2026-02-06 16:35:35 +0700

    test

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,8 @@ export default [
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "single"],
-      no-unused-vars: "warn",
-      no-console: "off"
+      "no-unused-vars": "warn",
+      "no-console": "off"
     }
   }
 ];
