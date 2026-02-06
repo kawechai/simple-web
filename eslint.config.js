4ff49a68f49ac0c8c5a8687c4939e0c51203c38e
kawechai.pu <kawechai.pu@planbmedia.co.th>
2026-02-06 16:30:40 +0700

    fix eslint problem

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ["web/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script"
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      no-unused-vars: "warn",
+      no-console: "off"
+    }
+  }
+];
